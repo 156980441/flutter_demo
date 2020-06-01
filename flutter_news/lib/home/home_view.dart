3cfe8345bf1458ec1ac442d5ef6d1e70f3e0df67
fanyl <yunlong.fan@okcoin.com>
2020-06-01 15:31:49 +0800

    【提交类型】new 【修改内容】提交文件，下拉刷新 【影响范围】bb 【风险评估】low

--- flutter_news/lib/home/home_view.dart
+++ flutter_news/lib/home/home_view.dart
@@ -9,88 +9,112 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-
+  ScrollController _scrollController;
   @override
   void initState() {
     super.initState();
+
+    _scrollController = ScrollController()
+      ..addListener(() {
+        if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent) {
+          print("loading");
+        }
+      });
+  }
+
+  Widget _buildSwiper(BuildContext context) {
+    return Container(
+      child: Swiper(
+        pagination: SwiperPagination(),
+        control: SwiperControl(),
+        autoplay: true,
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.orange,
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            child: Center(
+              child: Text(
+                "Digit Currency",
+                style: TextStyle(fontSize: 50),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+        },
+      ),
+      height: 150,
+    );
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Image.network(
+              "",
+              width: 130,
+              height: 110,
+            ),
+            color: Colors.grey,
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "BTC",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                margin: EdgeInsets.only(left: 10, top: 10, right: 10),
+                width: MediaQuery.of(context).size.width - 130 - 20,
+              ),
+              Container(
+                child: Text("ETH"),
+                margin: EdgeInsets.only(left: 10, top: 5),
+              ),
+              Container(
+                child: Text("EOS"),
+                margin: EdgeInsets.only(left: 10, top: 5),
+              ),
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+        ],
+      ),
+      height: 110,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(bottom: 1),
+      color: Colors.amber,
+    );
+  }
+
+  Future<Null> _onRefresh() async {
+    await Future.delayed(Duration(seconds: 3), () {
+      print("loading");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
-            return Container(
-              child: Swiper(
-                pagination: SwiperPagination(),
-                control: SwiperControl(),
-                autoplay: true,
-                itemCount: 3,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    color: Colors.orange,
-                    width: MediaQuery.of(context).size.width,
-                    height: 100,
-                    child: Center(
-                      child: Text(
-                        "Digit Currency",
-                        style: TextStyle(fontSize: 50),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  );
-                },
-              ),
-              height: 150,
-            );
-          } else {
-            return Container(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Image.network(
-                      "",
-                      width: 130,
-                      height: 110,
-                    ),
-                    color: Colors.grey,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          "BTC",
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        margin: EdgeInsets.only(left: 10, top: 10, right: 10),
-                        width: MediaQuery.of(context).size.width - 130 - 20,
-                      ),
-                      Container(
-                        child: Text("ETH"),
-                        margin: EdgeInsets.only(left: 10, top: 5),
-                      ),
-                      Container(
-                        child: Text("EOS"),
-                        margin: EdgeInsets.only(left: 10, top: 5),
-                      ),
-                    ],
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                  ),
-                ],
-              ),
-              height: 110,
-              width: MediaQuery.of(context).size.width,
-              margin: EdgeInsets.only(bottom: 1),
-              color: Colors.amber,
-            );
-          }
-        },
-        itemCount: 10,
-      ),
+      child: RefreshIndicator(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return _buildSwiper(context);
+              } else {
+                return _buildItem(context, index);
+              }
+            },
+            itemCount: 10,
+            controller: _scrollController,
+          ),
+          onRefresh: _onRefresh),
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       color: Colors.white,
