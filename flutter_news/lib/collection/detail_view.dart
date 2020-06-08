7eb96cd6d572264b2dd22fbe01cd85a6663faffa
fanyl <yunlong.fan@okcoin.com>
2020-06-08 11:45:58 +0800

    【提交类型】new 【修改内容】提交文件，收藏功能 【影响范围】bb 【风险评估】low

--- flutter_news/lib/collection/detail_view.dart
+++ flutter_news/lib/collection/detail_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_web/flutter_native_web.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailView extends StatefulWidget {
   String url;
@@ -21,9 +23,39 @@ class _DetailViewState extends State<DetailView> {
     super.initState();
   }
 
+  void _addCollection() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String data = prefs.get(this.url);
+    if (data == null) {
+      await prefs.setString(this.url, this.title);
+      Fluttertoast.showToast(msg: "收藏成功");
+    } else {
+      Fluttertoast.showToast(msg: "已经添加过收藏了");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    AppBar _appbar = AppBar(title:Text(title));
+    AppBar _appbar = AppBar(title:Text(title),
+    actions: <Widget>[
+      GestureDetector (
+        child: Container(
+          child: Icon(Icons.add),
+          width: 60,
+        ),
+        onTap: () {
+          showDialog(context: context, builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("确定添加到收藏吗"),
+              actions: <Widget>[
+                FlatButton(onPressed: () { _addCollection(); Navigator.of(context).pop();}, child: Text("确定")),
+                FlatButton(onPressed: () {Navigator.of(context).pop();}, child: Text("取消")),
+              ],
+            );
+          },);
+        },
+      ),
+    ],);
     return Scaffold(
       appBar: _appbar,
       body: Container(
