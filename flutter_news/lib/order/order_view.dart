1431115a3ba76d2797e1aae2038ce6f5950635c6
fanyl <yunlong.fan@okcoin.com>
2020-06-11 19:15:56 +0800

    【提交类型】new 【修改内容】订单 UI 【影响范围】bb 【风险评估】low

--- flutter_news/lib/order/order_view.dart
+++ flutter_news/lib/order/order_view.dart
@@ -52,76 +52,99 @@ class _OrderViewState extends State<OrderView> {
                       ),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      _orders[index].symbol,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 14, color: Colors.black),
+                  Container(
+                    margin: const EdgeInsets.only(left: 5, right: 15),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        _orders[index].symbol,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 14, color: Colors.black),
+                      ),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      _orders[index].ctime,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        _orders[index].ctime,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: 59,
+                    height: 24,
+                    child: FlatButton(
+                      color: Colors.white,
+                      textColor: Colors.blue,
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.grey,
+                      padding: EdgeInsets.all(2.0),
+                      splashColor: Colors.blueAccent,
+                      onPressed: () {},
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(fontSize: 12.0),
+                      ),
                     ),
                   ),
                 ],
               ),
-
-              Row(
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Amount ${_orders[index].amount}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 12, color: Colors.black),
+              Container(
+                margin: const EdgeInsets.only(top: 14),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Amount ${_orders[index].amount}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                          ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Filled ${_orders[index].filled}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 12, color: Colors.black),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Filled ${_orders[index].filled}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                          ),
                         ),
-                      ),
-
-                    ],
-                  ),
-
-                  Column(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Price ${_orders[index].price}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 12, color: Colors.black),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Price ${_orders[index].price}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                          ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Avg Price ${_orders[index].margin}',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 12, color: Colors.black),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Avg Price ${_orders[index].margin}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 12, color: Colors.black),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
           width: MediaQuery.of(context).size.width,
-          color: index % 2 == 0 ? Colors.orange : Colors.blueAccent,
-          height: 130,
+          color: Colors.orange,
+          height: 66,
         ),
         onTap: () {
           Navigator.push(
