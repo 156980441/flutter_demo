11d5cc806be4a0b89f8611b93767fc6061146482
fanyl <yunlong.fan@okcoin.com>
2020-06-16 17:33:10 +0800

    【提交类型】new 【修改内容】限价委托，接入交互的 TextField 【影响范围】bb 【风险评估】low

--- flutter_news/lib/trade/limit/limit_view.dart
+++ flutter_news/lib/trade/limit/limit_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutternews/trade/model/limit_commit_model.dart';
 
 class LimitView extends StatefulWidget {
@@ -11,10 +12,12 @@ class LimitView extends StatefulWidget {
 class _LimitViewState extends State<LimitView> {
   LimitCommitModel _commitModel = LimitCommitModel();
   TextEditingController _controller;
+
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
+    _controller.addListener(_handlePriceTextField);
   }
 
   void dispose() {
@@ -22,27 +25,58 @@ class _LimitViewState extends State<LimitView> {
     super.dispose();
   }
 
+  void _handlePriceTextField() {
+    print("Price text field: ${_controller.text}");
+  }
+
+  FlatButton _createFlatButton(String title) {
+    return FlatButton(
+      color: Color(0xFFF7F8FA),
+      textColor: Color(0xFF999999),
+      disabledColor: Color(0xFFF7F8FA),
+      disabledTextColor: Color(0xFF999999),
+      padding: EdgeInsets.all(2.0),
+      splashColor: Colors.blueAccent,
+      onPressed: () {},
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 12.0),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(top:5.0, bottom: 5,),
       height: 121,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          TextField(
-            controller: _controller,
-            onChanged: (String value) {},
-            onSubmitted: (String value) {},
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Price',
+          ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: 32, maxWidth: MediaQuery.of(context).size.width,),
+            child: TextField(
+              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+              controller: _controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Price',
+              ),
             ),
           ),
-          Text('Est.Value'),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Amount',
+          Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Text('Est.Value'),
+          ),
+          Container(
+            height: 32,
+            child: TextField(
+              onChanged: (String value) {},
+              onSubmitted: (String value) {},
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Amount',
+              ),
             ),
           ),
           Row(
@@ -51,70 +85,22 @@ class _LimitViewState extends State<LimitView> {
               ButtonTheme(
                 minWidth: 55,
                 height: 24,
-                child: FlatButton(
-                  color: Color(0xFFF7F8FA),
-                  textColor: Color(0xFF999999),
-                  disabledColor: Color(0xFFF7F8FA),
-                  disabledTextColor: Color(0xFF999999),
-                  padding: EdgeInsets.all(2.0),
-                  splashColor: Colors.blueAccent,
-                  onPressed: () {},
-                  child: Text(
-                    "10%",
-                    style: TextStyle(fontSize: 12.0),
-                  ),
-                ),
+                child: _createFlatButton('10%'),
               ),
               ButtonTheme(
                 minWidth: 55,
                 height: 24,
-                child: FlatButton(
-                  color: Color(0xFFF7F8FA),
-                  textColor: Color(0xFF999999),
-                  disabledColor: Color(0xFFF7F8FA),
-                  disabledTextColor: Color(0xFF999999),
-                  padding: EdgeInsets.all(2.0),
-                  splashColor: Colors.blueAccent,
-                  onPressed: () {},
-                  child: Text(
-                    "20%",
-                    style: TextStyle(fontSize: 12.0),
-                  ),
-                ),
+                child: _createFlatButton('20%'),
               ),
               ButtonTheme(
                 minWidth: 55,
                 height: 24,
-                child: FlatButton(
-                  color: Color(0xFFF7F8FA),
-                  textColor: Color(0xFF999999),
-                  disabledColor: Color(0xFFF7F8FA),
-                  disabledTextColor: Color(0xFF999999),
-                  padding: EdgeInsets.all(2.0),
-                  splashColor: Colors.blueAccent,
-                  onPressed: () {},
-                  child: Text(
-                    "50%",
-                    style: TextStyle(fontSize: 12.0),
-                  ),
-                ),
+                child: _createFlatButton('50%'),
               ),
               ButtonTheme(
                 minWidth: 55,
                 height: 24,
-                child: FlatButton(
-                  color: Color(0xFFF7F8FA),
-                  textColor: Color(0xFF999999),
-                  disabledColor: Color(0xFFF7F8FA),
-                  disabledTextColor: Color(0xFF999999),
-                  padding: EdgeInsets.all(2.0),
-                  splashColor: Colors.blueAccent,
-                  onPressed: () {},
-                  child: Text(
-                    "100%",
-                    style: TextStyle(fontSize: 12.0),
-                  ),
-                ),
+                child: _createFlatButton('100%'),
               ),
             ],
           ),
